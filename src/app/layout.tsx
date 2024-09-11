546021c158d496712f616c49eb4bece43d0ff639
babaygt <yigitingbaba@gmail.com>
2024-09-11 10:25:40 +0200

    Update project configuration and styling
    - Configure Next.js for authentication
    - Update dependencies
    - Add global styles and layout
    - Configure Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,41 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-})
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
 const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-})
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
-	title: {
-		template: '%s | What I Use',
-		default: 'What I Use',
-	},
-	description:
-		'What I Use is a website that allows you to showcase the tools and resources you use.',
-}
+  title: {
+    template: "%s | What I Use",
+    default: "What I Use",
+  },
+  description:
+    "What I Use is a website that allows you to showcase the tools and resources you use.",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
 }
